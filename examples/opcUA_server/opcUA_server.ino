8761eeab5176ad87e69aad51ef8560bf41801749
Martino Facchin <m.facchin@arduino.cc>
2024-03-08 18:15:26 +0100

    initial ocmmit

---
+++ examples/opcUA_server/opcUA_server.ino
@@ -0,0 +1,72 @@
+#include "PortentaEthernet.h"
+#include "Arduino_open62541.h"
+extern "C" {
+#include <open62541/server.h>
+#include <open62541/server_config_file_based.h>
+
+  int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+    tp->tv_sec = millis() / 1000;
+    tp->tv_nsec = (millis() % 1000) * 1000;
+    return 0;
+  }
+
+  int gethostname(char *str, size_t len) {
+    memcpy(str, "arduino", 7);
+    return 0;
+  }
+
+  UA_InterruptManager *UA_InterruptManager_new_POSIX(const UA_String eventSourceName) {
+    return nullptr;
+  }
+
+  UA_ConnectionManager *UA_ConnectionManager_new_POSIX_UDP(const UA_String eventSourceName) {
+    return nullptr;
+  }
+}
+
+REDIRECT_STDOUT_TO(Serial)
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+
+  Ethernet.begin();
+  Serial.print("Our IP is ");
+  Serial.println(Ethernet.localIP());
+
+  /* Create a server listening on port 4840 (default) */
+  UA_Server *server = UA_Server_new();
+  //UA_Server *server = UA_Server_newFromFile(UA_STRING_ALLOC("config.json"));
+
+  /* Add a variable node to the server */
+
+  /* 1) Define the variable attributes */
+  UA_VariableAttributes attr = UA_VariableAttributes_default;
+  attr.displayName = UA_LOCALIZEDTEXT("en-US", "the answer");
+  UA_Int32 myInteger = 42;
+  UA_Variant_setScalar(&attr.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+
+  /* 2) Define where the node shall be added with which browsename */
+  UA_NodeId newNodeId = UA_NODEID_STRING(1, "the.answer");
+  UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+  UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+  UA_NodeId variableType = UA_NODEID_NULL; /* take the default variable type */
+  UA_QualifiedName browseName = UA_QUALIFIEDNAME(1, "the answer");
+
+  /* 3) Add the node */
+  UA_Server_addVariableNode(server, newNodeId, parentNodeId,
+                            parentReferenceNodeId, browseName,
+                            variableType, attr, NULL, NULL);
+
+  /* Run the server (until ctrl-c interrupt) */
+  UA_StatusCode status = UA_Server_runUntilInterrupt(server);
+
+  /* Clean up */
+  UA_Server_delete(server);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ src/posix/eventloop_posix.h
@@ -0,0 +1,197 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    Copyright 2017 (c) Stefan Profanter, fortiss GmbH
+ *    Copyright 2021 (c) Christian von Arnim, ISW University of Stuttgart (for VDW and umati)
+ *    Copyright 2021 (c) Fraunhofer IOSB (Author: Julius Pfrommer)
+ *    Copyright 2021 (c) Fraunhofer IOSB (Author: Jan Hermes)
+ */
+
+#ifndef UA_EVENTLOOP_POSIX_H_
+#define UA_EVENTLOOP_POSIX_H_
+
+#include <open62541/config.h>
+#include <open62541/plugin/eventloop.h>
+
+#include "../eventloop_timer.h"
+#include "../eventloop_common.h"
+#include "../mp_printf.h"
+#include "../open62541_queue.h"
+
+_UA_BEGIN_DECLS
+
+#include <errno.h>
+
+/*********************/
+/* POSIX Definitions */
+/*********************/
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#define UA_IPV6 1
+#define UA_ERRNO errno
+#define UA_INTERRUPTED EINTR
+#define UA_AGAIN EAGAIN /* the same as wouldblock on nearly every system */
+#define UA_INPROGRESS EINPROGRESS
+#define UA_WOULDBLOCK EWOULDBLOCK
+#define UA_POLLIN POLLIN
+#define UA_POLLOUT POLLOUT
+#define UA_SHUT_RDWR SHUT_RDWR
+
+#define UA_clean_errno(STR_FUN) \
+    (errno == 0 ? (char*) "None" : (STR_FUN)(errno))
+#define UA_LOG_SOCKET_ERRNO_WRAP(LOG) \
+    { char *errno_str = UA_clean_errno(strerror); LOG; errno = 0; }
+#define UA_LOG_SOCKET_ERRNO_GAI_WRAP(LOG) \
+    { const char *errno_str = UA_clean_errno(gai_strerror); LOG; errno = 0; }
+
+
+/***********************/
+/* General Definitions */
+/***********************/
+
+#define UA_MAXBACKLOG 100
+#define UA_MAXHOSTNAME_LENGTH 256
+#define UA_MAXPORTSTR_LENGTH 6
+
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+
+#ifndef MSG_DONTWAIT
+#define MSG_DONTWAIT 0
+#endif
+
+/* POSIX events are based on sockets / file descriptors. The EventSources can
+ * register their fd in the EventLoop so that they are considered by the
+ * EventLoop dropping into "poll" to wait for events. */
+
+/* TODO: Move the macro-forest from /arch/<arch>/ua_architecture.h */
+
+#define UA_FD           uintptr_t //(Socket*)
+#define UA_INVALID_FD   0
+
+struct UA_RegisteredFD;
+typedef struct UA_RegisteredFD UA_RegisteredFD;
+
+/* Bitmask to be used for the UA_FDCallback event argument */
+#define UA_FDEVENT_IN 1
+#define UA_FDEVENT_OUT 2
+#define UA_FDEVENT_ERR 4
+
+typedef void (*UA_FDCallback)(UA_EventSource *es, UA_RegisteredFD *rfd, short event);
+
+struct UA_RegisteredFD {
+    UA_DelayedCallback dc; /* Used for async closing. Must be the first member
+                            * because the rfd is freed by the delayed callback
+                            * mechanism. */
+
+    ZIP_ENTRY(UA_RegisteredFD) zipPointers; /* Register FD in the EventSource */
+    UA_FD fd;
+    short listenEvents; /* UA_FDEVENT_IN | UA_FDEVENT_OUT*/
+
+    UA_EventSource *es; /* Backpointer to the EventSource */
+    UA_FDCallback eventSourceCB;
+};
+
+enum ZIP_CMP cmpFD(const UA_FD *a, const UA_FD *b);
+typedef ZIP_HEAD(UA_FDTree, UA_RegisteredFD) UA_FDTree;
+ZIP_FUNCTIONS(UA_FDTree, UA_RegisteredFD, zipPointers, UA_FD, fd, cmpFD)
+
+/* All ConnectionManager in the POSIX EventLoop can be cast to
+ * UA_ConnectionManagerPOSIX. They carry a sorted tree of their open
+ * sockets/file-descriptors. */
+typedef struct {
+    UA_ConnectionManager cm;
+
+    /* Statically allocated buffers */
+    UA_ByteString rxBuffer;
+    UA_ByteString txBuffer;
+
+    /* Sorted tree of the FDs */
+    size_t fdsSize;
+    UA_FDTree fds;
+} UA_POSIXConnectionManager;
+
+typedef struct {
+    UA_EventLoop eventLoop;
+
+    /* Timer */
+    UA_Timer timer;
+
+    /* Linked List of Delayed Callbacks */
+    UA_DelayedCallback *delayedCallbacks;
+
+    /* Flag determining whether the eventloop is currently within the
+     * "run" method */
+    UA_Boolean executing;
+
+#if defined(UA_ARCHITECTURE_POSIX) && !defined(__APPLE__) && !defined(__MACH__)
+    /* Clocks for the eventloop's time domain */
+    UA_Int32 clockSource;
+    UA_Int32 clockSourceMonotonic;
+#endif
+
+#if defined(UA_HAVE_EPOLL)
+    UA_FD epollfd;
+#else
+    UA_RegisteredFD **fds;
+    size_t fdsSize;
+#endif
+
+#if UA_MULTITHREADING >= 100
+    UA_Lock elMutex;
+#endif
+} UA_EventLoopPOSIX;
+
+/* The following functions differ between epoll and normal select */
+
+/* Register to start receiving events */
+UA_StatusCode
+UA_EventLoopPOSIX_registerFD(UA_EventLoopPOSIX *el, UA_RegisteredFD *rfd);
+
+/* Modify the events that the fd listens on */
+UA_StatusCode
+UA_EventLoopPOSIX_modifyFD(UA_EventLoopPOSIX *el, UA_RegisteredFD *rfd);
+
+/* Deregister but do not close the fd. No further events are received. */
+void
+UA_EventLoopPOSIX_deregisterFD(UA_EventLoopPOSIX *el, UA_RegisteredFD *rfd);
+
+UA_StatusCode
+UA_EventLoopPOSIX_pollFDs(UA_EventLoopPOSIX *el, UA_DateTime listenTimeout);
+
+/* Helper functions across EventSources */
+
+UA_StatusCode
+UA_EventLoopPOSIX_allocateStaticBuffers(UA_POSIXConnectionManager *pcm);
+
+UA_StatusCode
+UA_EventLoopPOSIX_allocNetworkBuffer(UA_ConnectionManager *cm,
+                                     uintptr_t connectionId,
+                                     UA_ByteString *buf,
+                                     size_t bufSize);
+
+void
+UA_EventLoopPOSIX_freeNetworkBuffer(UA_ConnectionManager *cm,
+                                    uintptr_t connectionId,
+                                    UA_ByteString *buf);
+
+/* Set the socket non-blocking. If the listen-socket is nonblocking, incoming
+ * connections inherit this state. */
+UA_StatusCode
+UA_EventLoopPOSIX_setNonBlocking(UA_FD sockfd);
+
+/* Don't have the socket create interrupt signals */
+UA_StatusCode
+UA_EventLoopPOSIX_setNoSigPipe(UA_FD sockfd);
+
+/* Enables sharing of the same listening address on different sockets */
+UA_StatusCode
+UA_EventLoopPOSIX_setReusable(UA_FD sockfd);
+
+_UA_END_DECLS
+
+#endif /* UA_EVENTLOOP_POSIX_H_ */
