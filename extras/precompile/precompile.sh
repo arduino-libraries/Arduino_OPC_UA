fe05c8435a93bbe184a97f53744d613edce79367
Alexander Entinger <consulting@lxrobotics.com>
2024-04-24 13:34:49 +0200

    Add comments during script execution.

--- extras/precompile/precompile.sh
+++ extras/precompile/precompile.sh
@@ -2,16 +2,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-cd /tmp
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+SRC_DIR="$SCRIPT_DIR/../../src"
+LIB_DIR="$SRC_DIR/cortex-m7"
 
+echo "Cleaning up"
+cd /tmp
 rm -rf open62541
+
+echo "Cloning open62541"
 git clone --recursive https://github.com/open62541/open62541 && cd open62541
 git checkout v1.4.0
 
+echo "Obtaining CMake toolchain definition for ARM"
 wget https://raw.githubusercontent.com/swift-nav/libsbp/master/c/cmake/Toolchain-gcc-arm-embedded.cmake
 
+echo "Build static library libopen62541.a"
 mkdir build && cd build
-
 cmake -DCMAKE_C_COMPILER=arm-none-eabi-gcc \
       -DCMAKE_SYSTEM_PROCESSOR=cortex-m7 \
       -DCMAKE_TOOLCHAIN_FILE=../Toolchain-gcc-arm-embedded.cmake\
@@ -20,5 +27,7 @@ cmake -DCMAKE_C_COMPILER=arm-none-eabi-gcc \
       -DUA_NAMESPACE_ZERO=REDUCED \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       ..
-
 make -j8
+
+echo "Copy precompiled library to Arduino library"
+cp bin/libopen62541.a "$LIB_DIR"
