2ab633870e81199eafe97edc762a66c5b85d3c2f
Martino Facchin <m.facchin@arduino.cc>
2024-05-07 17:50:26 +0200

    Implement fake interruptManager

--- examples/opcUA_server/opcUA_server.ino
+++ examples/opcUA_server/opcUA_server.ino
@@ -10,8 +10,33 @@ extern "C"
     return 0;
   }
 
+  UA_StatusCode registerFakeInterrupt(UA_InterruptManager *im, uintptr_t interruptHandle, const UA_KeyValueMap *params,  UA_InterruptCallback callback, void *interruptContext) {
+    return UA_STATUSCODE_GOOD;
+  }
+  void deregisterFakeInterrupt(UA_InterruptManager *im, uintptr_t interruptHandle) {
+    return;
+  }
+  UA_StatusCode startFakeInterruptManager(UA_EventSource *es) {
+    return UA_STATUSCODE_GOOD;
+  }
+  void stopFakeInterruptManager(UA_EventSource *es) {
+    return;
+  }
+  UA_StatusCode freeFakeInterruptManager(UA_EventSource *es) {
+    return UA_STATUSCODE_GOOD;
+  }
+
   UA_InterruptManager *UA_InterruptManager_new_POSIX(const UA_String eventSourceName) {
-    return nullptr;
+    static UA_InterruptManager im;
+    static UA_String name = UA_String_fromChars("fakeES");
+    im.eventSource.eventSourceType = UA_EVENTSOURCETYPE_INTERRUPTMANAGER;
+    UA_String_copy(&eventSourceName, &name);
+    im.eventSource.start = startFakeInterruptManager;
+    im.eventSource.stop = stopFakeInterruptManager;
+    im.eventSource.free = freeFakeInterruptManager;
+    im.registerInterrupt = registerFakeInterrupt;
+    im.deregisterInterrupt = deregisterFakeInterrupt;
+    return &im;
   }
 
   UA_ConnectionManager *UA_ConnectionManager_new_POSIX_UDP(const UA_String eventSourceName) {
