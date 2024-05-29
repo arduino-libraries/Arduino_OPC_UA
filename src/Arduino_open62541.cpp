a5d7d6adddacdc5c4b923481887d0bb8e1d13411
Alexander Entinger <consulting@lxrobotics.com>
2024-05-29 09:47:48 +0200

    Add method nodes for generic relay turn on/off.

--- src/Arduino_open62541.cpp
+++ src/Arduino_open62541.cpp
@@ -13,6 +13,28 @@
 
 #include "Arduino_open62541.h"
 
+#include <Arduino.h>
+
+/**************************************************************************************
+ * INTERNAL FUNCTION DECLARATION
+ **************************************************************************************/
+
+static UA_StatusCode
+relay_n_close_MethodCallback(UA_Server *server,
+                             const UA_NodeId *sessionId, void *sessionHandle,
+                             const UA_NodeId *methodId, void *methodContext,
+                             const UA_NodeId *objectId, void *objectContext,
+                             size_t inputSize, const UA_Variant *input,
+                             size_t outputSize, UA_Variant *output);
+
+static UA_StatusCode
+relay_n_open_MethodCallback(UA_Server *server,
+                            const UA_NodeId *sessionId, void *sessionHandle,
+                            const UA_NodeId *methodId, void *methodContext,
+                            const UA_NodeId *objectId, void *objectContext,
+                            size_t inputSize, const UA_Variant *input,
+                            size_t outputSize, UA_Variant *output);
+
 /**************************************************************************************
  * FUNCTION DEFINITIONS
  **************************************************************************************/
@@ -169,4 +191,84 @@ UA_StatusCode opc_ua_define_relay_obj(UA_Server * server,
                  UA_StatusCode_name(rc));
     return rc;
   }
+
+  UA_MethodAttributes relay_close_attr = UA_MethodAttributes_default;
+  relay_close_attr.description = UA_LOCALIZEDTEXT("en-US","Close the relay");
+  relay_close_attr.displayName = UA_LOCALIZEDTEXT("en-US","Close");
+  relay_close_attr.executable = true;
+  relay_close_attr.userExecutable = true;
+  rc = UA_Server_addMethodNode(server,
+                               UA_NODEID_NULL,
+                               *opta_relay_node_id,
+                               UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+                               UA_QUALIFIEDNAME(1, "close"),
+                               relay_close_attr, relay_n_close_MethodCallback,
+                               0, NULL, 0, NULL, NULL, NULL);
+  if (UA_StatusCode_isBad(rc))
+  {
+    UA_ServerConfig * config = UA_Server_getConfig(server);
+    UA_LOG_ERROR(config->logging,
+                 UA_LOGCATEGORY_SERVER,
+                 "UA_Server_addMethodNode(..., \"relay_n_close_func\", ...) failed with %s",
+                 UA_StatusCode_name(rc));
+    return rc;
+  }
+
+  UA_MethodAttributes relay_open_attr = UA_MethodAttributes_default;
+  relay_open_attr.description = UA_LOCALIZEDTEXT("en-US","Open the relay");
+  relay_open_attr.displayName = UA_LOCALIZEDTEXT("en-US","Open");
+  relay_open_attr.executable = true;
+  relay_open_attr.userExecutable = true;
+  rc = UA_Server_addMethodNode(server,
+                               UA_NODEID_NULL,
+                               *opta_relay_node_id,
+                               UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+                               UA_QUALIFIEDNAME(1, "open"),
+                               relay_open_attr, relay_n_open_MethodCallback,
+                               0, NULL, 0, NULL, NULL, NULL);
+  if (UA_StatusCode_isBad(rc))
+  {
+    UA_ServerConfig * config = UA_Server_getConfig(server);
+    UA_LOG_ERROR(config->logging,
+                 UA_LOGCATEGORY_SERVER,
+                 "UA_Server_addMethodNode(..., \"relay_n_open_MethodCallback\", ...) failed with %s",
+                 UA_StatusCode_name(rc));
+    return rc;
+  }
+}
+
+/**************************************************************************************
+ * INTERNAL FUNCTION DEFINITION
+ **************************************************************************************/
+
+UA_StatusCode
+relay_n_close_MethodCallback(UA_Server *server,
+                             const UA_NodeId *sessionId, void *sessionHandle,
+                             const UA_NodeId *methodId, void *methodContext,
+                             const UA_NodeId *objectId, void *objectContext,
+                             size_t inputSize, const UA_Variant *input,
+                             size_t outputSize, UA_Variant *output)
+{
+  /* Close the default-open switch. */
+  digitalWrite(RELAY1, HIGH);
+  digitalWrite(LED_D0, HIGH);
+  /* Write some debug output. */
+  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "\"relay_n_close_MethodCallback\" was called");
+  return UA_STATUSCODE_GOOD;
+}
+
+UA_StatusCode
+relay_n_open_MethodCallback(UA_Server *server,
+                            const UA_NodeId *sessionId, void *sessionHandle,
+                            const UA_NodeId *methodId, void *methodContext,
+                            const UA_NodeId *objectId, void *objectContext,
+                            size_t inputSize, const UA_Variant *input,
+                            size_t outputSize, UA_Variant *output)
+{
+  /* Close the default-open switch. */
+  digitalWrite(RELAY1, LOW);
+  digitalWrite(LED_D0, LOW);
+  /* Write some debug output. */
+  UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "\"relay_n_open_MethodCallback\" was called");
+  return UA_STATUSCODE_GOOD;
 }
